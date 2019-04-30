59aa9e22cbef03418a3abbfcf8a42cc4e562bd89
jakubkoje <you@example.com>
2019-04-30 22:03:54 +0200

    deploy

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	};
 
 	onSubmitSignIn = () => {
-		fetch("http://localhost:3000/register", {
+		fetch("https://obscure-ridge-86037.herokuapp.com/register", {
 			method: "post",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
